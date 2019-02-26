f4d9c9daa3fbf13a1db356b2243e9432a63f67f5
黄腾 <bobi@huangtengdeMacBook-Pro.local>
2019-02-26 22:44:47 +0800

    add

--- preact-learn-note/preact-source-code/lib/util.js
+++ preact-learn-note/preact-source-code/lib/util.js
@@ -5,6 +5,14 @@ export function extend(obj, props) {
   return obj
 }
 
+// 调用或更新 ref，具体取决于它是函数还是对象引用
+export const applyRef = (ref, value) => {
+  if(ref) {
+    if(typeof ref === 'function') ref(value)
+    else ref.current = value
+  }
+}
+
 // 尽快的调用异步函数
 // 使用 Promise 来安排回调（如果可用），否则就降级到 setTimeout
 export const defer = typeof Promise == 'function' ? Promise.resolve().then.bind(Promise.resolve()) : setTimeout
