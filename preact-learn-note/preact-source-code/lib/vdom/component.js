9035a7dd244872eb1491bc3ea712c2905e26fbdc
黄腾 <bobi@huangtengdeMacBook-Pro.local>
2019-02-27 23:24:03 +0800

    preact component

--- preact-learn-note/preact-source-code/lib/vdom/component.js
+++ preact-learn-note/preact-source-code/lib/vdom/component.js
@@ -1,5 +1,64 @@
-import { extend } from '../util'
-import { FORCE_RENDER } from '../constants'
+import { extend, applyRef } from '../util'
+import { FORCE_RENDER, SYNC_RENDER, NO_RENDER } from '../constants'
+import { getNodeProps } from './index'
+import options from '../options'
+import { enqueueRender } from '../render-queue'
+
+/**
+ * 设置一个组件的`props`并且可能重新渲染组件
+ * @param {import('../component').Component} component 需要设置 props 的组件
+ * @param {object} props 新的`props`
+ * @param {number} renderMode 渲染模式
+ * @param {object} context 新的 `context`
+ * @param {boolean} mountAll 是否立即挂载所有组件
+*/
+export function setComponentProps(component, props, renderMode, context, mountAll) {
+  // 锁
+  if(component._disable) return
+  component._disable = true
+  
+  component.__ref = props.ref
+  component.__key = props.key
+  delete props.ref
+  delete props.key
+
+  // 兼容新旧版本的 React 生命周期
+  if(typeof component.constructor.getDerivedStateFromProps === 'undefined') {
+    if(!component.base || mountAll) {
+      if(component.componentWillMount) component.componentWillMount()
+    }else if(component.componentWillReceiveProps) {
+      // https://reactjs.org/docs/react-component.html#unsafe_componentwillreceiveprops
+      component.componentWillReceiveProps(props, context);
+    }
+  }
+
+  // 更新组件的 context
+  if(context && context !== component.context) {
+    if(!component.prevContext)  component.prevContext = component.context
+    component.context = context
+  }
+
+  // 更新组件的 props
+  if(!component.prevProps) component.prevProps = props
+  component.props = props
+
+  // 锁在这儿中断
+  component._disable = false
+
+  // 触发重新渲染
+  if(renderMode !== NO_RENDER) {
+    // 同步更新
+    if(renderMode === SYNC_RENDER || options.syncComponentUpdates !== false || !component.base) {
+      renderComponent(component, SYNC_RENDER, mountAll)
+    }else {
+      // 异步队列更新
+      enqueueRender(component)
+    }
+  }
+
+  // 设置 ref
+  applyRef(component.__ref, component)
+}
 
 /**
  * 渲染组件，触发必要的生命周期事件并考虑高阶组件
@@ -62,5 +121,38 @@ export function renderComponent(component, renderMode, mountAll, isChild) {
     // 返回一个新的 v-node
     rendered = component.render(props, state, context)
     
+    // 传递给子组件的上下文，可以通过（祖先）父组件更新
+    if(component.getChildContext) {
+      context = extend(extend({}, context), component.getChildContext())
+    }
+
+    // https://reactjs.org/docs/react-component.html#getsnapshotbeforeupdate
+    if(isUpdate && component.getSnapshotBeforeUpdate) {
+      snapshot = component.getSnapshotBeforeUpdate(previousProps, previousState)
+    }
+
+    let childComponent = rendered && rendered.nodeName, toUnmount, base
+
+    // 如果 nodeName 是一个函数（h(nodeName, attributes)）
+    if(typeof childComponent === 'function') {
+      // 设置高阶组件连接
+      let childProps = getNodeProps(rendered)
+      inst = initialChildComponent
+
+      if(inst && inst.constructor === childComponent && childProps.key == inst.__key) {
+        setComponentProps(inst, childProps, SYNC_RENDER, context, false)
+      }else {
+        toUnmount = inst
+
+        component._component = inst = createComponent(childComponent, childProps, context)
+        inst.nextBase = inst.nextBase || nextBase
+        inst._parentComponent = component
+        setComponentProps(inst, childProps, NO_RENDER, context, false)
+        renderComponent(inst, SYNC_RENDER, mountAll, true)
+      }
+      base = inst.base
+    }else {
+      // todo
+    }
   }
 }
